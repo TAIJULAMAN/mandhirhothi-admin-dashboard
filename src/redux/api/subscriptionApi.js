4c073fb73cc2f4c05810bd9800a6248a43160a74
Hasan <158465711+Ahteshamull@users.noreply.github.com>
2025-08-19 03:55:51 +0600

    Subscription page intrigration Done

--- src/redux/api/subscriptionApi.js
+++ src/redux/api/subscriptionApi.js
@@ -1,26 +1,24 @@
 import { baseApi } from "./baseApi";
 
 export const subscriptionApi = baseApi.injectEndpoints({
-          endpoints: (builder) => ({
-                    getAllSubscription: builder.query({
-                              query: () => ({
-                                        url: "package/get-all",
-                                        method: "GET",
-                              }),
-                              providesTags: ["subscription"],
-                    }),
-                    addFeature: builder.mutation({
-                              query: (data) => ({
-                                        url: "package/create",
-                                        method: "POST",
-                                        body: data
-                              }),
-                              invalidatesTags: ["subscription"],
-                    }),
-          }),
+  endpoints: (builder) => ({
+    getAllSubscription: builder.query({
+      query: () => ({
+        url: "subscription/find_all_buyer_subscription",
+        method: "GET",
+      }),
+      providesTags: ["subscription"],
+    }),
+    addFeature: builder.mutation({
+      query: (data) => ({
+        url: "package/create",
+        method: "POST",
+        body: data,
+      }),
+      invalidatesTags: ["subscription"],
+    }),
+  }),
 });
 
-export const {
-          useGetAllSubscriptionQuery,
-          useAddFeatureMutation,
-} = subscriptionApi;
+export const { useGetAllSubscriptionQuery, useAddFeatureMutation } =
+  subscriptionApi;
